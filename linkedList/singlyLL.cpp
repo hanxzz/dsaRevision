d649b3cb72f7c4d14aba1683a7a4594ed1321a35
Han <variety1174@gmail.com>
2025-08-21 23:11:00 +0530

    updates

--- linkedList/singlyLL.cpp
+++ linkedList/singlyLL.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<map>
 using namespace std;
 
 class Node{
@@ -37,6 +38,48 @@ void print(Node* &head){
     cout << endl;
 }
 
+
+bool detectLoop(Node* head){
+    if(head == NULL){
+        return false;
+    }
+    map<Node* , bool> visited;
+
+    Node*temp = head;
+    while(temp!= NULL){
+        if(visited[temp] == true){
+            return true;
+        }
+        visited[temp] = true;
+        temp = temp -> next;
+    }
+
+    return false;
+
+}
+
+Node* floydDetectLoop(Node* head){
+    if(head == NULL){
+        return NULL;
+    }
+    Node* slow = head;
+    Node* fast = head;
+
+    while(slow!=NULL && fast!= NULL){
+        fast = fast ->next;
+        if(fast!=NULL){
+            fast = fast ->next;
+        }
+        slow = slow -> next;
+
+        if(slow==fast){
+            cout<<"Cycle is present at "<< slow->data<<endl;
+            return slow;
+        }
+    }
+    return NULL;
+}
+
 int main(){
     Node* node1 = new Node(10);
     //cout << node1 -> data << endl;
@@ -50,6 +93,18 @@ int main(){
     insertAtTail(tail, 12);
     print(head);
 
-    insertAtTail(tail, 15   );
+    insertAtTail(tail, 15);
     print(head);
+    insertAtTail(tail , 30);
+    print(head);
+
+    tail->next = head -> next;
+
+
+    if(floydDetectLoop(head)){
+        cout << "Cycle is Present" <<endl;
+    }
+    else{
+        cout << "No cycle is present"<<endl;
+    }
 }
